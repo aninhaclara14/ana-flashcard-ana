3b9e2290ebc5aa7c4f817fe51c588bfd19290e32
aninhaclara14 <a.camargos@escola.pr.gov.br>
2025-06-03 11:34:39 +0000

    aula2

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,9 @@ criaCartao(
     'Qual é o animal mais rápido do mundo?',
     'Falcão Peregrino'
 )
+criaCartao(
+    'Animais',
+    'Qual é o tempo de gestação de uma leoa',
+    'Entre 4 e 5 meses'
+)
+
